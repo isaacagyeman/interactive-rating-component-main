8e8335f468d63f874409a4354837ddde79d2b0b5
unknown <isaacagyeman437@gmail.com>
2023-03-28 09:39:13 +0100

    fixed thank you card

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,23 @@
 var rateButtons = document.getElementsByClassName('btn-rate')
+var submitButton = document.querySelector('.btnSubmit')
+var rateSelected = document.querySelector('#numSelected')
 
-let rateNumber
-for(var i = 0; i < rateButtons.length; i++){
-    rateButtons[i].addEventListener('click',()=>{
-        // rateButtons[i].style.background = 'hsl( hsl(217, 12%, 63%))'
-        rateButtons[i].style.display = 'none'
-        rateNumber = rateButtons[i].innerHTML
-        console.log(rateNumber)
-    })
-}
+
+
+let rateNumber=0;
+
+rateButtons[1].addEventListener('click',()=>{
+rateNumber = rateButtons[1].innerHTML
+rateButtons[1].style.background = 'hsl(217,12%,63%)'
+rateButtons[1].style.color = 'hsl(0, 0%, 100%)'
+})
+
+submitButton.addEventListener('click',()=>{
+    if(rateNumber != 0){
+    document.querySelector('.cardRate').style.display = 'none'
+    document.querySelector('.cardThanks').style.display = 'block'
+    rateSelected.innerHTML = 'You selected '+ rateNumber +' out of 5'
+    }
+})
+
+
